b5c00652ebc906b3f8224702c7d22d1a33681ed9
Felipe Brunet <felipe.brunet@gmail.com>
2024-11-30 16:04:59 -0300

    flippin aye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from binascii import hexlify, unhexlify
+from hashlib import sha256, sha512
 from Crypto.Hash import keccak
 import nacl.bindings
 
@@ -25,10 +26,11 @@ inv = nacl.bindings.crypto_core_ed25519_scalar_invert
 scalar_add = nacl.bindings.crypto_core_ed25519_scalar_add
 scalarmult_B = nacl.bindings.crypto_scalarmult_ed25519_base_noclamp
 scalarmult = nacl.bindings.crypto_scalarmult_ed25519_noclamp
+
 def scalar_reduce(v):
     return nacl.bindings.crypto_core_ed25519_scalar_reduce(v + (64 - len(v)) * b"\0")
 
-def keccak_hash(data):
+def keccak_hash(data:  str):
     k = keccak.new(digest_bits=256)
     k.update(data)
     return k.digest()
@@ -52,26 +54,37 @@ def get_address_final(baseaddress, secret_vk, major, minor):
             minor.to_bytes(4, byteorder='little'),
         ]
     )
+    
     m = keccak_hash(hsdata)
     # D = master_psk + m * B
     D = edwards_add(
         master_psk, scalarmult_B(scalar_reduce(m))
     )
+  
     # C = master_svk * D
     C = scalarmult(master_svk, D)
+    
     data = bytearray(b'*') + D + C
     checksum = keccak_hash(data)[:4]
+
+
+    print('m en hex es:                ', m.hex())
+    print('el escalar reducido de m es:', scalar_reduce(m).hex())
+    # print(scalarmult_B(scalar_reduce(m)))
+    # print(D)
+    
+
     return base58.encode(hexlify(data + checksum))
 
 
 
 
 
-print(get_address_final(baseaddress, secret_vk, 0, 11))
+# print(get_address_final(baseaddress, secret_vk, 0, 6))
+acoaco = get_address_final(baseaddress, secret_vk, 0, 6)
 
+# print(base58.decode(baseaddress)[2:66])
 
-print(base58.decode(baseaddress)[2:66])
-
-aco = b'holahola'
-print(hexlify(keccak_hash(aco)).decode())
+# aco = b'holahola'
+# print(hexlify(keccak_hash(aco)).decode())
 
